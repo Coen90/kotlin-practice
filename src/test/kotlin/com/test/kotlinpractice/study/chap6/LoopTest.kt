58847dfdb79d88669c118fdff1b5b83d04a793af
Coen90 <bht9011@gmail.com>
2026-07-09 08:54:54 +0900

    feat: add chap 6

---
+++ src/test/kotlin/com/test/kotlinpractice/study/chap6/LoopTest.kt
@@ -0,0 +1,28 @@
+package com.test.kotlinpractice.study.chap6
+
+import kotlin.test.Test
+
+class LoopTest {
+    @Test
+    fun test1() {
+        val numbers = listOf(1L, 2L, 3L)
+        for (number in numbers) {
+            println(number)
+        }
+        for (i in 1..3) {
+            println(i)
+        }
+        for (i in 3 downTo 1) {
+            println(i)
+        }
+        for (i in 1..5 step 2) {
+            println(i)
+        }
+
+        var i = 1
+        while (i <= 3) {
+            println(i)
+            i++
+        }
+    }
+}
